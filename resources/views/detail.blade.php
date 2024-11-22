4e28532d58e31847aa231d04228096cece6e7087
DarklLionRRR <lllevaa28@gmail.com>
2024-11-22 18:46:24 +0300

    link Bbs to User

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,6 +4,7 @@
     <h2>{{ $bb->title }}</h2>
 
     <p>{{ $bb->content }}</p>
+    <p>Автор: {{ $bb->user->name }}</p>
     <p>{{ $bb->price }} руб.</p>
     <p><a href="{{ route('index') }}">На перечень объявлений</a></p>
 </x-layouts.app>
