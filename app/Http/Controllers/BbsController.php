059e19750efb134efedb8ef652000edd96de49c7
DarklLionRRR <lllevaa28@gmail.com>
2024-11-22 17:43:34 +0300

    add controller for Bb

---
+++ app/Http/Controllers/BbsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bb;
+use Illuminate\Http\Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class BbsController extends Controller
+{
+    public function index(): ViewFactory|View
+    {
+        return view('index', ['bbs' => Bb::latest()->get()]);
+    }
+
+    public function detail(Bb $bb): ViewFactory|View
+    {
+        return view('detail', ['bb' => $bb]);
+    }
+}
